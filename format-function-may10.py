47a4cda4306f4c6ff20c49ae5c201404c2fdf8cd
Marsh100 <80366504+Marsh100@users.noreply.github.com>
2021-05-10 10:56:01 +1200

    Add files via upload

---
+++ format-function-may10.py
@@ -0,0 +1,17 @@
+name = input("What is your name?")
+age = input("How old are you?")
+ethnicity = input("What is your ethnicity?")
+
+print("{} is a nice age to be, {}! {} is a lovely race".format(age,name,ethnicity))
+
+food = input("What is your favourite food?")
+print("{} is yum!".format(food))
+
+colour = input("What is your favourite colour?")
+print("{} is a cool colour!".format(colour))
+
+birth = input("What is your date of birth?")
+print("nice!")
+
+siblings = input("How many siblings do you have?")
+print("cool!")
