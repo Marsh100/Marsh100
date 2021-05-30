aebe5c8d753fb57b14b5e945e3763c8d399e0e73
Marsh100 <80366504+Marsh100@users.noreply.github.com>
2021-05-31 10:34:49 +1200

    Update format-function-may10.py

--- format-function-may10.py
+++ format-function-may10.py
@@ -1,20 +1,26 @@
+#persons personal descriptions
 name = input("What is your name?")
 age = input("How old are you?")
 ethnicity = input("What is your ethnicity?")
 
 print("{} is a nice age to be, {}! {} is a lovely race".format(age,name,ethnicity))
 
+#persons favourite food
 food = input("What is your favourite food?")
 print("{} is yum!".format(food))
 
+#persons favourite colour
 colour = input("What is your favourite colour?")
 print("{} is a cool colour!".format(colour))
 
+#persons date of birth
 birth = input("What is your date of birth?")
 print("nice!")
 
+#persons siblings
 siblings = input("How many siblings do you have?")
 print("cool!")
 
+#persons address
 address = input("What is your address?")
 print("im coming to {}")
